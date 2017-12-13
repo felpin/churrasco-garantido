12c131be38389334686a1a97a9091a40da30410c
Felipe Pinheiro <felipepinheiro@labtrans.ufsc.br>
2017-12-13 15:46:12 -0200

    Removes unused code

--- routes/account.js
+++ routes/account.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 const Joi = require('joi');
 const service = require('../services/account');
 
-router.get('/', (req, res) => {
-  res.sendStatus(204);
-});
-
 router.post('/create', (req, res) => {
   const bodySchema = Joi.object({
     username: Joi.string().email().required(),

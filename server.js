098e6a6339b33ffb0683f6ccfa73bd975150218c
Felipe Pinheiro <felipepinheiro@labtrans.ufsc.br>
2017-12-13 11:06:22 -0200

    Connects to the database before start the server

--- server.js
+++ server.js
@@ -3,18 +3,32 @@ require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const mongoose = require('mongoose');
 
-const app = express();
-
-const httpsOptions = {
-  cert: fs.readFileSync(process.env.SERVER_SSL_CRT),
-  key: fs.readFileSync(process.env.SERVER_SSL_KEY),
+const mongooseOptions = {
+  keepAlive: true,
+  useMongoClient: true,
 };
 
-const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.DB_CONNECTION_STRING, mongooseOptions);
 
-https
-  .createServer(httpsOptions, app)
-  .listen(PORT, () => {
-    console.log(`***** LISTENING ON PORT ${PORT} *****`);
-  });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'There was an error connecting to the database:'));
+db.once('open', whenDatabaseConnectionOpens);
+
+function whenDatabaseConnectionOpens() {
+  const app = express();
+
+  const httpsOptions = {
+    cert: fs.readFileSync(process.env.SERVER_SSL_CRT),
+    key: fs.readFileSync(process.env.SERVER_SSL_KEY),
+  };
+
+  const PORT = process.env.PORT || 3000;
+
+  https
+    .createServer(httpsOptions, app)
+    .listen(PORT, () => {
+      console.log(`***** LISTENING ON PORT ${PORT} *****`);
+    });
+}

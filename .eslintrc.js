ca0a09cb487d497bd7cac73d8cd9aaf60b2336e9
Felipe Pinheiro <felipepinheiro@labtrans.ufsc.br>
2017-12-13 11:06:50 -0200

    Changes a rule in eslint since there is hoisting in JS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+        "node": true
+    },
+    "rules": {
+        "no-use-before-define": ["error", { "functions": false }]
+    }
 };

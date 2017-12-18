06ece061d2bea62bb4298a14e710690eb7aaf138
Felipe Pinheiro <felipepinheiro@labtrans.ufsc.br>
2017-12-18 17:28:35 -0200

    Change the name of the routes to be more restful

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 router.use('/account', require('./account'));
-router.use('/company', require('./company'));
-router.use('/order', require('./order'));
-router.use('/product', require('./product'));
+router.use('/companies', require('./company'));
+router.use('/orders', require('./order'));
+router.use('/products', require('./product'));
 
 module.exports = router;
